4ded314aee2f8de2636f6d0676afb6d74356c022
Deepak <deepak@deepaks-macbook-pro.local>
2019-12-09 19:17:50 +0530

    Uebook

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'UeBook' do
+use_frameworks!
+pod 'Alamofire', '4.4.0'
+
+end
+
---
+++ Pods/Target Support Files/Pods-UeBook/Pods-UeBook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UeBook : NSObject
+@end
+@implementation PodsDummy_Pods_UeBook
+@end
